6ab36f278605efb18b0c8f05960bbc0647660cdf
Marc Plano-Lesay <marc.planolesay@gmail.com>
2012-02-04 22:05:07 +0100

    Added a first time wizard

--- src/com/buildbotwatcher/BuildbotWatcherActivity.java
+++ src/com/buildbotwatcher/BuildbotWatcherActivity.java
@@ -5,9 +5,14 @@ import com.buildbotwatcher.worker.JsonParser;
 import com.buildbotwatcher.worker.Project;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -18,23 +23,50 @@ public class BuildbotWatcherActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 
-		Button bSettings = (Button) findViewById(R.id.btn_settings);
+		linkBtns();
 
-		bSettings.setOnClickListener(new View.OnClickListener() {
-			public void onClick(View v) {
-				Intent i = new Intent();
-				i.setClass(BuildbotWatcherActivity.this, SettingsActivity.class);
-				startActivity(i);
-			}
-		});
-
+		firstTimeWizard();
 
 		JsonParser p = new JsonParser("http://buildbot.buildbot.net", 80);
-		//Log.d("Project", p.getProject().getName());
 
 		new GetProject().execute(p);
 
 	}
+
+	private void firstTimeWizard() {
+		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
+		String host = prefs.getString("host", "");
+		if (host.equals("")) {
+			AlertDialog.Builder popup = new AlertDialog.Builder(this);
+			popup.setTitle(R.string.ftw_title);
+			popup.setMessage(R.string.ftw_message);
+			popup.setPositiveButton(R.string.ftw_now, new OnClickListener() {
+				public void onClick(DialogInterface dialog, int which) {
+					startSettings();
+				}
+			});
+			popup.setNegativeButton(R.string.ftw_later, null);
+			popup.create();
+			popup.show();
+		}
+
+	}
+
+	private void linkBtns() {
+		Button bSettings = (Button) findViewById(R.id.btn_settings);
+		bSettings.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				startSettings();
+			}
+		});
+	}
+
+	private void startSettings() {
+		Intent i = new Intent();
+		i.setClass(BuildbotWatcherActivity.this, SettingsActivity.class);
+		startActivity(i);
+	}
+
 
 	private class GetProject extends AsyncTask<JsonParser, Integer, Project> {
 		protected Project doInBackground(JsonParser... p) {
