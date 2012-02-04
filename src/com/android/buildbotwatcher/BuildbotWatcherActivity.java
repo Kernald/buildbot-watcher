4bcd562ff2c06473a0a7569929087efc6cfb5a98
Marc Plano-Lesay <marc.planolesay@gmail.com>
2012-02-04 20:21:49 +0100

    Creation of an async task to get the project name

--- src/com/android/buildbotwatcher/BuildbotWatcherActivity.java
+++ src/com/android/buildbotwatcher/BuildbotWatcherActivity.java
@@ -1,8 +1,10 @@
 package com.android.buildbotwatcher;
 
 import com.android.buildbotwatcher.worker.JsonParser;
+import com.android.buildbotwatcher.worker.Project;
 
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 
@@ -13,6 +15,23 @@ public class BuildbotWatcherActivity extends Activity {
         setContentView(R.layout.main);
         
         JsonParser p = new JsonParser("http://buildbot.buildbot.net", 80);
-        Log.d("Project", p.getProject().getName());
+        //Log.d("Project", p.getProject().getName());
+        
+        new GetProject().execute(p);
+
+    }
+    
+    private class GetProject extends AsyncTask<JsonParser, Integer, Project> {
+        protected Project doInBackground(JsonParser... p) {
+            return p[0].getProject();
+        }
+
+        protected void onProgressUpdate(Integer... progress) {
+            // TODO
+        }
+
+        protected void onPostExecute(Project result) {
+        	Log.d("Project", result.getName());
+        }
     }
 }
