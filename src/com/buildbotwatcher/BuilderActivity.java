4ed89d730a216ffec29fcf662eb23c474ed84593
Marc Plano-Lesay <marc.planolesay@gmail.com>
2012-02-06 12:06:07 +0100

    Added the Builder's state

--- src/com/buildbotwatcher/BuilderActivity.java
+++ src/com/buildbotwatcher/BuilderActivity.java
@@ -2,20 +2,26 @@ package com.buildbotwatcher;
 
 import com.buildbotwatcher.worker.Builder;
 
-import android.app.Activity;
+import android.app.ListActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.widget.ListView;
+import android.widget.TextView;
 
-public class BuilderActivity extends Activity {
+public class BuilderActivity extends ListActivity {
 	private Builder	_builder;
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 	    super.onCreate(savedInstanceState);
 	    setContentView(R.layout.builder);
-	    Bundle bundle = this.getIntent().getExtras();
-	    Log.d("bundle content", "" + bundle);
+	    Bundle bundle = getIntent().getExtras();
 	    _builder = (Builder) bundle.get("builder");
 	    setTitle(_builder.getName());
+	    TextView state = (TextView) findViewById(R.id.state);
+	    state.setText(String.format(getResources().getString(R.string.builder_state), _builder.getState()));
+	    TextView header = (TextView) getLayoutInflater().inflate(R.layout.builder_list_header, null);
+	    int count = _builder.getBuilds().size();
+	    header.setText(getResources().getQuantityString(R.plurals.builder_build_number, count, count));
+		ListView listView = getListView();
+		listView.addHeaderView(header);
 	}
-
 }
