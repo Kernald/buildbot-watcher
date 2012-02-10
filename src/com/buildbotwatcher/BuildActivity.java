8d5fded3ab54bb2d25368c9db8bb0304c054c775
Marc Plano-Lesay <marc.planolesay@gmail.com>
2012-02-10 23:47:17 +0100

    Added refresh to build details

--- src/com/buildbotwatcher/BuildActivity.java
+++ src/com/buildbotwatcher/BuildActivity.java
@@ -1,19 +1,25 @@
 package com.buildbotwatcher;
 
 import java.sql.Time;
+import java.util.List;
 
 import com.buildbotwatcher.worker.Build;
 import com.buildbotwatcher.worker.Builder;
+import com.buildbotwatcher.worker.JsonParser;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.TextView;
 
 public class BuildActivity extends Activity {
 	private Build	_build;
 	private Builder	_builder;
+	private Menu	_menu;
 
 	/** Called when the activity is first created. */
 	@Override
@@ -28,6 +34,38 @@ public class BuildActivity extends Activity {
 		Bundle bundle = getIntent().getExtras();
 		_build = (Build) bundle.get("build");
 		_builder = (Builder) bundle.get("builder");
+		setupUi();
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		_menu = menu;
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.builders, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case android.R.id.home:
+			Intent intent = new Intent(this, BuilderActivity.class);
+			intent.putExtra("builder", _builder);
+			intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+			startActivity(intent);
+			return true;
+			
+		case R.id.menu_refresh:
+			item.setEnabled(false);
+			refresh();
+			return true;
+			
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+	}
+	
+	private void setupUi() {
 		setTitle(String.valueOf(_build.getNumber()));
 		TextView res = (TextView) findViewById(R.id.result);
 		if (_build.getText().containsKey("build"))
@@ -43,18 +81,25 @@ public class BuildActivity extends Activity {
 		Time duration = new Time(_build.getTimeEnd().getTime() - _build.getTimeStart().getTime());
 		((TextView) findViewById(R.id.duration)).setText(String.format("%02d:%02d", duration.getMinutes(), duration.getSeconds()));
 	}
+	
+	private void refresh() {
+		_menu.findItem(R.id.menu_refresh).setEnabled(false);
+		new RefreshBuild().execute();
+	}
+	
+	private class RefreshBuild extends AsyncTask<Void, Integer, Build> {
+		protected Build doInBackground(Void... v) {
+			return _builder.getBuild(_build.getNumber(), true);
+		}
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		switch (item.getItemId()) {
-		case android.R.id.home:
-			Intent intent = new Intent(this, BuilderActivity.class);
-			intent.putExtra("builder", _builder);
-			intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-			startActivity(intent);
-			return true;
-		default:
-			return super.onOptionsItemSelected(item);
+		protected void onProgressUpdate(Integer... progress) {
+			// TODO
+		}
+
+		protected void onPostExecute(Build result) {
+			_build = result;
+			setupUi();
+			_menu.findItem(R.id.menu_refresh).setEnabled(true);
 		}
 	}
 }
