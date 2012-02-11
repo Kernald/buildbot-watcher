2ee0eb59d8dd0acbcc32c7f3ca0cb4fedab0e78c
Marc Plano-Lesay <marc.planolesay@gmail.com>
2012-02-11 17:29:40 +0100

    Remove unused imports

--- src/com/buildbotwatcher/worker/JsonParser.java
+++ src/com/buildbotwatcher/worker/JsonParser.java
@@ -19,7 +19,6 @@ import javax.net.ssl.HostnameVerifier;
 import javax.net.ssl.HttpsURLConnection;
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.SSLSession;
-import javax.net.ssl.SSLSocketFactory;
 import javax.net.ssl.TrustManager;
 import javax.net.ssl.X509TrustManager;
 
